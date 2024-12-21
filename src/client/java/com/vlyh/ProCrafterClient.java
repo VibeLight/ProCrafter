c64360035bfd35f97931e3ff902dcc9a7e8f234a
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-21 19:42:18 +0900

    Add files via upload

---
+++ src/client/java/com/vlyh/ProCrafterClient.java
@@ -0,0 +1,10 @@
+package com.vlyh;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProCrafterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
