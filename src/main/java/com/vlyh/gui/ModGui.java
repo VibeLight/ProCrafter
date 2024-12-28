fd78cce16433f2fad952186cbc701d3a04638966
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 08:51:09 +0900

    vlyh の更新

---
+++ src/main/java/com/vlyh/gui/ModGui.java
@@ -0,0 +1,15 @@
+package com.vlyh.procrafter.gui;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.text.Text;
+
+public class ModGui extends Screen {
+    protected ModGui(Text title) {
+        super(title);
+    }
+
+    @Override
+    protected void init() {
+        // GUI要素の追加
+    }
+}
