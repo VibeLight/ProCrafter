88737904df378c58ee1fe7e23054654512666f07
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 08:32:26 +0900

    Update ProCrafterMod.java

--- src/main/java/com/vlyh/ProCrafterMod.java
+++ src/main/java/com/vlyh/ProCrafterMod.java
@@ -1,18 +1,1 @@
-package com.vlyh.procrafter;
 
-import com.vlyh.procrafter.registry.ModBlocks;
-import com.vlyh.procrafter.registry.ModItems;
-import net.fabricmc.api.ModInitializer;
-
-public class ProCrafterMod implements ModInitializer {
-    public static final String MOD_ID = "procrafter";
-
-    @Override
-    public void onInitialize() {
-        // アイテムやブロックの登録
-        ModItems.registerItems();
-        ModBlocks.registerBlocks();
-
-        System.out.println("ProCrafter API initialized!");
-    }
-}
