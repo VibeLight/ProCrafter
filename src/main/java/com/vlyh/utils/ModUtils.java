20d65ba7cd225dc07385413702f80154cce0bae5
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 08:53:01 +0900

    vlyh の更新

---
+++ src/main/java/com/vlyh/utils/ModUtils.java
@@ -0,0 +1,9 @@
+package com.vlyh.procrafter.utils;
+
+import net.minecraft.util.Identifier;
+
+public class ModUtils {
+    public static Identifier id(String name) {
+        return new Identifier("procrafter", name);
+    }
+}
