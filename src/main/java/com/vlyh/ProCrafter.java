3873e75bf2eade4309db30e70bd8b5c8c0bf8c51
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 08:33:38 +0900

    ProCrafter.java の更新

--- src/main/java/com/vlyh/ProCrafter.java
+++ src/main/java/com/vlyh/ProCrafter.java
@@ -1,24 +1,18 @@
-package com.vlyh;
+package com.vlyh.procrafter;
 
+import com.vlyh.procrafter.registry.ModBlocks;
+import com.vlyh.procrafter.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class ProCrafterMod implements ModInitializer {
+    public static final String MOD_ID = "procrafter";
 
-public class ProCrafter implements ModInitializer {
-	public static final String MOD_ID = "procrafter";
+    @Override
+    public void onInitialize() {
+        // アイテムやブロックの登録
+        ModItems.registerItems();
+        ModBlocks.registerBlocks();
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+        System.out.println("ProCrafter API initialized!");
+    }
 }
