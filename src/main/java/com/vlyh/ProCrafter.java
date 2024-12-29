6f250bc19d2ea3100de4376044d5c4944dde483b
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 09:39:34 +0900

    ProCrafter.java の更新

--- src/main/java/com/vlyh/ProCrafter.java
+++ src/main/java/com/vlyh/ProCrafter.java
@@ -3,6 +3,7 @@ package com.vlyh.procrafter;
 import com.vlyh.procrafter.registry.ModBlocks;
 import com.vlyh.procrafter.registry.ModItems;
 import com.vlyh.procrafter.registry.ModEntities;
+import com.vlyh.procrafter.registry.ModDimensions;
 import com.vlyh.procrafter.registry.ModSound;
 import net.fabricmc.api.ModInitializer;
 
@@ -14,6 +15,9 @@ public class ProCrafterMod implements ModInitializer {
         // レジストリを初期化
         ModItems.registerItems();
         ModBlocks.registerBlocks();
+        ModEntities.registerEntities();
+        ModSounds.registerSounds();
+        ModDimensions.registerDimensions();
         ModBlockEntities.registerBlockEntities();
         ModScreenHandlers.registerScreenHandlers();
     }
