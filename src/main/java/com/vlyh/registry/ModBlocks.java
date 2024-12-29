bd032ad9cd36493f55cfebd5ac3feea55a5fa073
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 20:00:44 +0900

    Update ModBlocks.java

--- src/main/java/com/vlyh/registry/ModBlocks.java
+++ src/main/java/com/vlyh/registry/ModBlocks.java
@@ -9,7 +9,7 @@ import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
     public static final Block MAGIC_MACHINE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(3.0f));
-    
+    public static final Block CABBAGE = new CustomCropBlock(FabricBlockSettings.copyOf(Blocks.WHEAT));
     public static final BlockEntityType<MagicMachineBlockEntity> MAGIC_MACHINE_ENTITY = Registry.register(
         Registry.BLOCK_ENTITY_TYPE,
         new Identifier("procrafter", "magic_machine"),
@@ -19,12 +19,7 @@ public class ModBlocks {
     public static void registerBlocks() {
         Registry.register(Registry.BLOCK, new Identifier("procrafter", "magic_machine_block"), MAGIC_MACHINE_BLOCK);
         Registry.register(Registry.ITEM, new Identifier("procrafter", "magic_machine_block"), new BlockItem(MAGIC_MACHINE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
-    }
-    public static final Block CABBAGE = new CustomCropBlock(FabricBlockSettings.copyOf(Blocks.WHEAT));
-
-    public static void registerBlocks() {
         Registry.register(Registries.BLOCK, new Identifier("procrafter", "cabbage"), CABBAGE);
-    }
-    
+    }  
 
 }
