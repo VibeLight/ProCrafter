f94092181dea7890f7c28efe92a91b655dcefa2d
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 09:12:25 +0900

    registry の更新

---
+++ src/main/java/com/vlyh/registry/ModBlocks.java
@@ -0,0 +1,23 @@
+package com.vlyh.procrafter.registry;
+
+import com.vlyh.procrafter.blocks.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.item.BlockItem;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModBlocks {
+    public static final Block MAGIC_MACHINE_BLOCK = new Block(FabricBlockSettings.of(Material.STONE).strength(3.0f));
+    
+    public static final BlockEntityType<MagicMachineBlockEntity> MAGIC_MACHINE_ENTITY = Registry.register(
+        Registry.BLOCK_ENTITY_TYPE,
+        new Identifier("procrafter", "magic_machine"),
+        BlockEntityType.Builder.create(MagicMachineBlockEntity::new, MAGIC_MACHINE_BLOCK).build(null)
+    );
+    
+    public static void registerBlocks() {
+        Registry.register(Registry.BLOCK, new Identifier("procrafter", "magic_machine_block"), MAGIC_MACHINE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier("procrafter", "magic_machine_block"), new BlockItem(MAGIC_MACHINE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
+    }
+}
