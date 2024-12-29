26ab15be7160c9ac28a71a4159a1e8bc53751ebf
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-29 19:33:42 +0900

    Update ModItems.java

--- src/main/java/com/vlyh/registry/ModItems.java
+++ src/main/java/com/vlyh/registry/ModItems.java
@@ -11,13 +11,17 @@ import net.minecraft.util.registry.Registry;
 public class ModItems {
     public static final Item MAGIC_DUST = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
     public static final Item NEOMAGIC_DUST = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
-    public static final Item MAGIC_SWORD = new SwordItem(ToolMaterials.IRON, 10, -2.4f, new FabricItemSettings().group(ItemGroup.COMBAT));
     public static final Item GRINGHAM_SMALLORE = new Item(new FabricItemSettings().group(ItemGroup.MATERIALS));
+    public static final Item MAGIC_SWORD = new SwordItem(ToolMaterials.IRON, 10, -2.4f, new FabricItemSettings().group(ItemGroup.COMBAT));
+    public static final Item MAGIC_CHOCO = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(6).saturationModifier(1.2f).alwaysEdible().build()));
+    public static final Item CABBAGE = new PlaceableCropItem(ModBlocks.CABBAGE, new FabricItemSettings().group(ItemGroup.MATERIALS));
     
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("procrafter", "magic_dust"), MAGIC_DUST);
         Registry.register(Registry.ITEM, new Identifier("procrafter", "neomagic_dust"), NEOMAGIC_DUST);
         Registry.register(Registry.ITEM, new Identifier("procrafter", "magic_sword"), MAGIC_SWORD);
         Registry.register(Registry.ITEM, new Identifier("procrafter","gringham_smallore"),GRINGHAM_SMALLORE);
+        Registry.register(Registry.TIME, new Identifier("procrafter","magic_choco"),MAGIC_CHOCO);
+        Registry.register(Registry.ITEM, new Identifier("procrafter","cabbage"),CABBAGE);
     }
 }
