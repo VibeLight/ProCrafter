2657cb1118e2a9fa80316f2b1803e8972be1a0f2
Yuyuto000 <yukkurigame0101@gmail.com>
2024-12-30 16:20:11 +0900

    Update ModItems.java

--- src/main/java/com/vlyh/registry/ModItems.java
+++ src/main/java/com/vlyh/registry/ModItems.java
@@ -15,6 +15,9 @@ public class ModItems {
     public static final Item MAGIC_SWORD = new SwordItem(ToolMaterials.IRON, 10, -2.4f, new FabricItemSettings().group(ItemGroup.COMBAT).maxCount(1));
     public static final Item MAGIC_CHOCO = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(6).saturationModifier(1.2f).alwaysEdible().build()).maxCount(64));
     public static final Item CABBAGE = new PlaceableCropItem(ModBlocks.CABBAGE, new FabricItemSettings().group(ItemGroup.MATERIALS).maxCount(64));
+    public static final Item FIREBALL_MAGICBOOK = new Item(new FAbricItemSettings().group(ItemGroup.MATERIALS).maxCount(64);
+    public static final Item WATER_MAGICBOOK = new Item(new FabricItemSettings().group(ItemGroup.MATERIAL).maxCount(64));
+
     
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier("procrafter", "magic_dust"), MAGIC_DUST);
@@ -23,5 +26,7 @@ public class ModItems {
         Registry.register(Registry.ITEM, new Identifier("procrafter","gringham_smallore"),GRINGHAM_SMALLORE);
         Registry.register(Registry.TIME, new Identifier("procrafter","magic_choco"),MAGIC_CHOCO);
         Registry.register(Registry.ITEM, new Identifier("procrafter","cabbage"),CABBAGE);
+        Registry.register(Registry.ITEM, new Identifier("procrafter","fireball_magicbook"),FIREBALL_MAGICBOOK);
+        Registry.register(Registry.ITEM, new Identifier("procrafter","water_magicball"),WATER_MAGICBOOK);
     }
 }
